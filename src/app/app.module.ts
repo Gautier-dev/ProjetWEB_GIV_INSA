f4525c56e788cc3f301fc6baccdc910b8cc3bdaf
Yarkane <samuel.jacquier@insa-lyon.fr>
2019-04-30 17:44:12 +0200

    30/04 : Liens MVC effectifs. Un seul module : liste des utilisateurs. Todolist : - Routing - Api FLASK Complète

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListeUtilisateursComponent } from './liste-utilisateurs/liste-utilisateurs.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeUtilisateursComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
