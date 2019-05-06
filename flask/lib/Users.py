3d1fd1dbc758d28a435eeaee50041fd218977b87
bonnappart <bonnegaut@gmail.com>
2019-05-06 17:24:59 +0200

    Flask authentification

---
+++ flask/lib/Users.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, jsonify, session, redirect, escape, url_for
+import bcrypt
+
+
+class ServerError(Exception):
+  pass
+
+def loginForm(db, form):
+    error = None
+    try:
+        idUser = form['idUser']
+        cur = db.query("SELECT COUNT(*) FROM utilisateurs WHERE nom = %s", [idUser])# verifier []
+
+        if not cur.fetchone()[0]:
+            raise ServerError('Incorrect username / password')
+
+        password = form['password']
+        cur = db.query("SELECT password FROM utilisateurs WHERE nom = %s", [idUser])
+
+        for row in cur.fetchall():
+            if bcrypt.hashpw(password.encode('utf-8'), row[0]) == row[0]: #TODO : voir
+                session['idUser'] = form['idUser']# on associe idUser a sa valeur dans une session, si la session est modifiee session.modified = True
+                return error
+
+        raise ServerError('Incorrect username / password')
+    except ServerError as e:
+      error = str(e)
+      return error
+
+def registerUser(db, form, ROUNDS):
+    error = None
+    try:
+      username = form['idUser']
+      password = form['password']
+      email    = form['mail']
+
+      if not username or not password or not email:
+        raise ServerError('Fill in all fields')
+
+      password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(ROUNDS))
+
+      cur = db.query("SELECT COUNT(*) FROM users WHERE user = %s", [username])
+      c = cur.fetchone()
+      if c[0] == 0:
+        cur = db.query("INSERT INTO utilisateur (`idUser`, `mail`, `password`) VALUES (%s,%s,%s)", [username, email, password])
+        return None
+      else:
+        return "User exists"
+    except ServerError as e:
+      error = str(e)
+      return error
+
+def getUsers(db):
+    error = None
+    try:
+        userlist = []
+        cur = db.query("SELECT idUser, mail FROM utilisateurs")
+        for row in cur.fetchall():
+            userlist.append({'name': row[0], 'email': row[1]})
+        return userlist
+    except:
+        error = "Failed"
+        return error
+
+def deleteUser(db, user):
+    error = None
+    try:
+      cur = db.query("DELETE FROM utilisateur WHERE nom = %s", [user])
+      return None
+    except:
+      return "Failed"
--- flask/serverFlask.py
+++ flask/serverFlask.py
@@ -1,45 +1,92 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, session
 from json import dumps
 from flask_jsonpify import jsonify
 import pymysql
+import lib.Users as Users
+
 
 app = Flask(__name__)
 api = Api(app)
 
 CORS(app)
+class ServerError(Exception): pass
 
 class Database:
     def __init__(self):
         #Initialisation de la connexion à la base de données
-        host = "localhost"
-        user = "root"
-        password = "123"
-        db = "givinsa"
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+        config = {}
+        execfile("config.conf", config) #TODO : erreur a verifier
+
+        host = config['db_host']
+        user = config['db_user']
+        password = config['db_pass']
+        db = config['db_data']
+        self.conn = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
                                    DictCursor)
-        self.cur = self.con.cursor()
+        self.conn.autocommit(True)
+        self.conn.set_character_set('utf8')
+
 
     #METHODES D'ACCES A LA BASE DE DONNEES
 
-    def list_users(self):
-        #Récupérer la liste des utilisateurs
-        self.cur.execute("SELECT * FROM utilisateurs")
-        result = self.cur.fetchall()
-        return result
 
 
-@app.route("/")
+    def query(self, sql, args=None):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql, args)
+        except (AttributeError, pymysql.OperationalError): # gerer les erreurs 
+            #__init__()
+            cursor = self.conn.cursor()
+            cursor.execute(sql, args)
+        return cursor #fetchall pour retourner des tuples
+
+"""
 class Utilisateurs(Resource):
     def get(self):
         db = Database()
         emps = db.list_users()
         json = jsonify(emps)
-        return json
+        return json """
+
+class login(Resource):
+  def get(self):
+    message = None
+    global notifications
+    if notifications:
+      message = notifications
+      notifications = None
+    if 'idUser' not in session:
+      message = {'message': 'Please log in', 'type': 'warning'}
+      return jsonify("/login")# URL de login a utiliser sur angular
+
+    return jsonify("envoie des donnees de l'utilisateur")# Si l'utilisateur est dans la session
+
+class Utilisateurs(Resource):
+  def get(self):
+    message = None
+    global notifications
+    if notifications:
+      message = notifications
+      notifications = None
+    if 'idUser' not in session:
+      notifications = {'message': 'Please log in', 'type': 'warning'}
+      return jsonify("/login")
+    if session['idUser'] != 'admin':
+      return jsonify("PageErreurAdmin")
+    db = Database()
+    users = Users.getUsers(db)
+    if not users:
+      notifications = {'message': 'Failed to retrieve users', 'type': 'error'}
+      return jsonify("PageErreurPasUser")
+    return jsonify("PageTouslesUtilisateurs")
+
 
 
 api.add_resource(Utilisateurs, '/utilisateurs') # Route_1
+api.add_resource(login, '/login')
 
 if __name__ == '__main__':
      app.run(port=5002)
